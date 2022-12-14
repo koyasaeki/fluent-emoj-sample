71ddb8e0d91630a15e91503747a78ee7a4963b5c
koyasaeki <koyasaeki@gmail.com>
2022-12-14 17:20:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "fluent-emoji-sample",
+  base: "sample.fluentemoji",
   build: {
     outDir: "dist"
   },
